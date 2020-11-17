72f7a2481cced9b813d090e66c3a2620d6edaf89
Yulia Klyueva <liya-n@list.ru>
2020-11-17 16:52:21 +0300

    решение пункта 5 Team Generation

--- src/js/generators.js
+++ src/js/generators.js
@@ -8,8 +8,19 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  while (true) {
+    const type = Math.floor(Math.random() * allowedTypes.length);
+    const level = Math.floor(Math.random() * maxLevel);
+    yield new allowedTypes[type](level);
+  }
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const team = [];
+  const unit = characterGenerator(allowedTypes, maxLevel);
+  for (let i = 0; i < characterCount; i += 1) {
+    team.push(unit.next().value);
+  }
+  return team;
 }
