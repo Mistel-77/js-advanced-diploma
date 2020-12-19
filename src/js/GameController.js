824dd16dbe7e082c749e79436f88f94d1198d09c
Yulia Klyueva <liya-n@list.ru>
2020-12-19 22:09:48 +0300

    Ура, персонажи появились на поле

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -7,12 +7,21 @@ import Magician from './characters/Magician';
 import Swordsman from './characters/Swordsman';
 import Undead from './characters/Undead';
 import Vampire from './characters/Vampire';
-import { generateTeam } from './generators';
+import PositionedCharacter from './PositionedCharacter';
+import { characterGenerator, generateTeam } from './generators';
 
 const playerClass = [Bowman, Magician, Swordsman];
 const computerClass = [Undead, Vampire, Daemon];
-const playerCell = [0, 1, 8, 9, 16, 17, 24, 25, 32, 33, 40, 41, 48, 49, 56, 57];
-const computerCell = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63];
+const playerTeam = generateTeam(playerClass, 1, 2);
+const computerTeam = generateTeam(computerClass, 1, 2);
+const playerCellOne = [0, 8, 16, 24, 32, 40, 48, 56];
+const playerCellTwo = [1, 9, 17, 25, 33, 41, 49, 57];
+const computerCellOne = [6, 14, 22, 30, 38, 46, 54, 62];
+const computerCellTwo = [7, 15, 23, 31, 39, 47, 55, 63];
+// eslint-disable-next-line max-len
+const playerTeamPosition = [new PositionedCharacter(playerTeam[0], playerCellOne[Math.floor(Math.random() * 8)]), new PositionedCharacter(playerTeam[1], playerCellTwo[Math.floor(Math.random() * 8)])];
+// eslint-disable-next-line max-len
+const computerTeamPosition = [new PositionedCharacter(computerTeam[0], computerCellOne[Math.floor(Math.random() * 8)]), new PositionedCharacter(computerTeam[1], computerCellTwo[Math.floor(Math.random() * 8)])];
 
 export default class GameController {
   constructor(gamePlay, stateService) {
@@ -34,8 +43,9 @@ export default class GameController {
 
     this.gamePlay.drawUi(this.theme); // отрисовка поля
     // this.gamePlay.redrawPositions(); // отрисовка команд
-    this.gamePlay.redrawPositions(this.generateTeam(playerCell, 1, playerClass));
-    this.gamePlay.redrawPositions(this.generateTeam(computerCell, 1, computerClass));
+    // this.gamePlay.redrawPositions(this.generateTeam(playerCell, 1, playerClass));
+    // this.gamePlay.redrawPositions(this.generateTeam(computerCell, 1, computerClass));
+    this.gamePlay.redrawPositions([...playerTeamPosition, ...computerTeamPosition]);
   }
 
   // eslint-disable-next-line no-unused-vars
